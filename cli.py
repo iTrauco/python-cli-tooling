205c1b6d7acd977f1f25be45c5bc5e29f6fa715c
trauco-dev <chris@trau.co>
2020-12-17 02:03:00 -0500

    added decorator over main function, not output = valid

---
+++ cli.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import click
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+@click.command(context_settings=CONTEXT_SETTINGS) def main():
+return
+if __name__ == '__main__': main()
